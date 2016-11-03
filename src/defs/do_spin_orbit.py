e25b88b8676e7ce9ae0cd5c3372ecb132a6dfbce
root <root@aflowlib.unt>
2016-11-03 15:34:25 -0500

    Updated to master

---
+++ src/defs/do_spin_orbit.py
@@ -0,0 +1,39 @@
+#
+# AflowPI_TB
+#
+# Utility to construct and operate on TB Hamiltonians from the projections of DFT wfc on the pseudoatomic orbital basis (PAO)
+#
+# Copyright (C) 2016 ERMES group (http://ermes.unt.edu)
+# This file is distributed under the terms of the
+# GNU General Public License. See the file `License'
+# in the root directory of the present distribution,
+# or http://www.gnu.org/copyleft/gpl.txt .
+#
+#
+# References:
+# Luis A. Agapito, Andrea Ferretti, Arrigo Calzolari, Stefano Curtarolo and Marco Buongiorno Nardelli,
+# Effective and accurate representation of extended Bloch states on finite Hilbert spaces, Phys. Rev. B 88, 165127 (2013).
+#
+# Luis A. Agapito, Sohrab Ismail-Beigi, Stefano Curtarolo, Marco Fornari and Marco Buongiorno Nardelli,
+# Accurate Tight-Binding Hamiltonian Matrices from Ab-Initio Calculations: Minimal Basis Sets, Phys. Rev. B 93, 035104 (2016).
+#
+# Luis A. Agapito, Marco Fornari, Davide Ceresoli, Andrea Ferretti, Stefano Curtarolo and Marco Buongiorno Nardelli,
+# Accurate Tight-Binding Hamiltonians for 2D and Layered Materials, Phys. Rev. B 93, 125137 (2016).
+#
+from scipy import fftpack as FFT
+import numpy as np
+import cmath
+import sys, time
+from mpi4py import MPI
+
+sys.path.append('./')
+
+from zero_pad import zero_pad
+
+comm=MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+def do_spin_orbit():
+
+
+	return()
