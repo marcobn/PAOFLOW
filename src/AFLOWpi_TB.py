correct units for AHC

src/AFLOWpi_TB.py
@@ -517,10 +517,12 @@ if Berry:
     ahc = do_Berry_curvature(E_k,pksp,nk1,nk2,nk3,delta,temp,ibrav,alat,a_vectors,b_vectors,dkres,nthread,npool)
 
     alat *= ANGSTROM_AU
+    omega = alat**3 * np.dot(a_vectors[0,:],np.cross(a_vectors[1,:],a_vectors[2,:]))
 
     if rank == 0:
         f=open('ahc.dat','w')
-        ahc *= AU_TO_OHMCMM1
+        #ahc *= AU_TO_OHMCMM1
+        ahc *= 1.0e8*ELECTRONVOLT_SI**2/H_OVER_TPI/omega
         f.write(' Anomalous Hall conductivity sigma_xy = %.6f\n' %ahc)
         f.close()
 

src/defs/do_Berry_curvature.py
@@ -44,7 +44,7 @@ size = comm.Get_size()
 
 def do_Berry_curvature(E_k,pksp,nk1,nk2,nk3,delta,temp,ibrav,alat,a_vectors,b_vectors,dkres,nthread,npool):
     #----------------------
-    # Compute Berry curvature on a selected path in the BZ
+    # Compute Berry curvature
     #----------------------
 
     index = None
@@ -93,12 +93,13 @@ def do_Berry_curvature(E_k,pksp,nk1,nk2,nk3,delta,temp,ibrav,alat,a_vectors,b_ve
         comm.Scatter(E_k_long,E_kaux,root=0)
 
         ########NOTE The indeces of the polarizations (x,y,z) should be changed according to the direction of the magnetization
-        deltap = 0.01
+        ########     Here we enforce explicitly the antisymmetry of the conductivity tensor to minimize convergence errors.
+        deltap = 0.05
         for nk in range(nsize):
             for n in range(nawf):
                 for m in range(nawf):
                     if m!= n:
-                        Om_znkaux[nk,n] += -2.0*np.imag(pksaux[nk,2,n,m,0]*pksaux[nk,1,m,n,0]- pksaux[nk,1,n,m,0]*pksaux[nk,2,m,n,0]) / \
+                        Om_znkaux[nk,n] += -1.0*np.imag(pksaux[nk,2,n,m,0]*pksaux[nk,1,m,n,0]- pksaux[nk,1,n,m,0]*pksaux[nk,2,m,n,0]) / \
                         ((E_kaux[nk,m,0] - E_kaux[nk,n,0])**2 + deltap**2)
         comm.Barrier()
         comm.Gather(Om_znkaux,Om_znk_split,root=0)

src/defs/do_velocity_calc.py
@@ -74,13 +74,15 @@ def do_velocity_calc(dHRaux,E_k,v_kp,R,ibrav,alat,a_vectors,b_vectors,dkres):
 
     # Compute Berry curvature
     ########NOTE The indeces of the polarizations (x,y,z) should be changed according to the direction of the magnetization
-    deltab = 0.01
+    deltab = 0.05
     Om_znk = np.zeros((nkpi,nawf),dtype=float)
     Om_zk = np.zeros((nkpi),dtype=float)
     for ik in range(nkpi):
         for n in range(nawf):
             for m in range(nawf):
                 if m!= n:
+                    #Om_znk[ik,n] += -2.0*np.imag(pks[ik,2,n,m,0]*pks[ik,1,m,n,0]) / \
+                    #((E_k[ik,m,0] - E_k[ik,n,0])**2 + deltab**2)
                     Om_znk[ik,n] += -2.0*np.imag(pks[ik,2,n,m,0]*pks[ik,1,m,n,0]-pks[ik,1,n,m,0]*pks[ik,2,m,n,0]) / \
                     ((E_k[ik,m,0] - E_k[ik,n,0])**2 + deltab**2)
         Om_zk[ik] = np.sum(Om_znk[ik,:]*(0.5 * (-np.sign(E_k[ik,:,0]) + 1)))  # T=0.0K