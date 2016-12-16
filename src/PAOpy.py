corrected units for sigxyi

src/PAOpy.py
@@ -462,7 +462,7 @@ if Berry:
         sigxy *= 1.0e8*ANGSTROM_AU*ELECTRONVOLT_SI**2/H_OVER_TPI/omega
         f=open('sigxyi.dat','w')
         for n in xrange(ene.size):
-            f.write('%.5f %9.5e \n' %(ene[n],np.imag(ene[n]*sigxy[n])))
+            f.write('%.5f %9.5e \n' %(ene[n],np.imag(ene[n]*sigxy[n]/105.4571)))  #convert energy in freq (1/hbar in cgs units)
         f.close()
         f=open('sigxyr.dat','w')
         for n in xrange(ene.size):