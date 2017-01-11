bf231b0cb956a925e7a3dc3b0538849fc5a77a0d
lsiliyanage <lsiliyanage@gmail.com>
2017-01-11 11:00:51 +0530

    Modified read_QE_output_xml_parse.py to read projections in a spin-polarized calculation

--- examples/test_xml_parse/read_QE_output_xml_parse.py
+++ examples/test_xml_parse/read_QE_output_xml_parse.py
@@ -1,7 +1,7 @@
 #
-# AFLOWpi_TB
+# PAOpy
 #
-# Utility to construct and operate on TB Hamiltonians from the projections of DFT wfc on the pseudoatomic orbital basis (PAO)
+# Utility to construct and operate on Hamiltonians from the Projections of DFT wfc on Atomic Orbital bases (PAO)
 #
 # Copyright (C) 2016 ERMES group (http://ermes.unt.edu)
 # This file is distributed under the terms of the
@@ -20,15 +20,24 @@
 # Luis A. Agapito, Marco Fornari, Davide Ceresoli, Andrea Ferretti, Stefano Curtarolo and Marco Buongiorno Nardelli,
 # Accurate Tight-Binding Hamiltonians for 2D and Layered Materials, Phys. Rev. B 93, 125137 (2016).
 #
+from __future__ import print_function
 import numpy as np
 import xml.etree.cElementTree as ET
 import sys
 import re
 
+from mpi4py import MPI
+from mpi4py.MPI import ANY_SOURCE
+
+# initialize parallel execution                                                                                                               
+comm=MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
 #units
 Ry2eV   = 13.60569193
 
-def read_QE_output_xml(fpath):
+def read_QE_output_xml(fpath,verbose,non_ortho):
     atomic_proj = fpath+'/atomic_proj.xml'
     data_file   = fpath+'/data-file.xml'
     read_S = False
@@ -40,7 +49,7 @@ def read_QE_output_xml(fpath):
             if elem.tag == "CELL":
                 alatunits  = elem.findall("LATTICE_PARAMETER")[0].attrib['UNITS']
                 alat   = float(elem.findall("LATTICE_PARAMETER")[0].text.split()[0])
-                print("The lattice parameter is: alat= {0:f} ({1:s})".format(alat,alatunits))
+                if rank == 0 and verbose: print("The lattice parameter is: alat= {0:f} ({1:s})".format(alat,alatunits))
 
                 aux=elem.findall("DIRECT_LATTICE_VECTORS/a1")[0].text.split()
                 a1=np.array(aux,dtype="float32")
@@ -73,35 +82,37 @@ def read_QE_output_xml(fpath):
                 k1=int(elem.findall("MONKHORST_PACK_OFFSET")[0].attrib['k1'])
                 k2=int(elem.findall("MONKHORST_PACK_OFFSET")[0].attrib['k2'])
                 k3=int(elem.findall("MONKHORST_PACK_OFFSET")[0].attrib['k3'])
+                if rank == 0 and verbose: print('Monkhorst&Pack grid',nk1,nk2,nk3,k1,k2,k3)
                 elem.clear()
 
-                print('Monkhorst&Pack grid',nk1,nk2,nk3,k1,k2,k3)
-
+            # number of atoms
+            if elem.tag == 'IONS':
+                natoms=int(float(elem.findall("NUMBER_OF_ATOMS")       [0].text.split()[0]))
 
     # Reading atomic_proj.xml
 
     group_nesting = 0
-    readEigVals = False; readProj = False
+    readEigVals = False; readProj = False; spincalc = False
     for event,elem in ET.iterparse(atomic_proj,events=('start','end')):
         if event == 'end' and  elem.tag == "HEADER":
             nkpnts = int(elem.findall("NUMBER_OF_K-POINTS")[0].text.strip())
-            print('Number of kpoints: {0:d}'.format(nkpnts))
+            if rank == 0 and verbose: print('Number of kpoints: {0:d}'.format(nkpnts))
 
             nspin  = int(elem.findall("NUMBER_OF_SPIN_COMPONENTS")[0].text.split()[0])
-            print('Number of spin components: {0:d}'.format(nspin))
+            if rank == 0 and verbose: print('Number of spin components: {0:d}'.format(nspin))
 
             kunits = elem.findall("UNITS_FOR_K-POINTS")[0].attrib['UNITS']
 
             nbnds  = int(elem.findall("NUMBER_OF_BANDS")[0].text.split()[0])
-            print('Number of bands: {0:d}'.format(nbnds))
+            if rank == 0 and verbose: print('Number of bands: {0:d}'.format(nbnds))
 
             aux    = elem.findall("UNITS_FOR_ENERGY")[0].attrib['UNITS']
 
             Efermi = float(elem.findall("FERMI_ENERGY")[0].text.split()[0])*Ry2eV
-            print('Fermi energy: {0:f} eV '.format(Efermi))
+            if rank == 0 and verbose: print('Fermi energy: {0:f} eV '.format(Efermi))
 
             nawf   =int(elem.findall("NUMBER_OF_ATOMIC_WFC")[0].text.split()[0])
-            print('Number of atomic wavefunctions: {0:d}'.format(nawf))
+            if rank == 0 and verbose: print('Number of atomic wavefunctions: {0:d}'.format(nawf))
 
 
             elem.clear()
@@ -128,7 +139,6 @@ def read_QE_output_xml(fpath):
                 my_eigsmat = np.zeros((nbnds,nkpnts,nspin))
             if elem.tag == "PROJECTIONS":
                 group_nesting += 1
-                ispin = 0
                 readProj = True
             if "K-POINT" in elem.tag:
                 if readProj :
@@ -137,27 +147,30 @@ def read_QE_output_xml(fpath):
             if "SPIN" in elem.tag:
 		if readProj:
 		    group_nesting=3
-
         if event == 'end':
             #Read eigen values for each k-point
             if "K-POINT" in elem.tag and group_nesting == 1 and readEigVals: # EIGENVALUES/K-POINT.{0:d}
                 ik = int(float(elem.tag.split('.')[-1]))-1
                 if nspin ==1:
                     ispin = 0
-                    eigk_type=elem.findall("EIG")[0].attrib['type']
-                    eigk_file=np.array(elem.findall("EIG")[0].text.split(),dtype='float32')
+                    subelem=elem.findall("EIG")[0]
+		    if verbose: print("Reading", subelem.tag,"of k-point", ik)
+		    eigk_type=subelem.attrib['type']
+                    eigk_file=np.array(subelem.text.split(),dtype='float32')
                     my_eigsmat[:,ik,ispin] = np.real(eigk_file)*Ry2eV-Efermi #meigs in eVs and wrt Ef
 
                 else:
-                    for ispin in range(nspin):
-                        eigk_type=elem.findall("EIG.%d"%(ispin+1))[0].attrib['type']
-                        eigk_file=np.array(elem.findall("EIG.%d"%(ispin+1))[0].text.split(),dtype='float32')
+                    for ispin in xrange(nspin):
+			subelem = elem.findall("EIG.%d"%(ispin+1))[0]
+		    	if verbose: print("Reading", subelem.tag,"spin component", ispin, "of k-point", ik)
+                        eigk_type=subelem.attrib['type']
+                        eigk_file=np.array(subelem.text.split(),dtype='float32')
                         my_eigsmat[:,ik,ispin] = np.real(eigk_file)*Ry2eV-Efermi #meigs in eVs and wrt Ef
 
 
-            if 'ATMWFC' in elem.tag and readProj and group_nesting == 2 : #PROJECTIONS/K-POINT.{0:d}/ATMWFC.{1:d}
-                ispin == 0
-		#print "Reading", elem.tag, "spin component", ispin
+            if 'ATMWFC' in elem.tag and readProj and nspin == 1: #PROJECTIONS/K-POINT.{0:d}/ATMWFC.{1:d}
+                ispin == nspin-1
+		if verbose:print("Reading", elem.tag, "of non-spin polarized calc") 
                 iin = int(float(elem.tag.split('.')[-1]))-1
                 wfc_type=elem.attrib['type']
                 aux     =elem.text
@@ -174,10 +187,11 @@ def read_QE_output_xml(fpath):
 
 	    if 'SPIN' in elem.tag and readProj and group_nesting ==3:
 		ispin = int(float(elem.tag.split('.')[-1]))-1
-		#print "SPIN tag component", ispin, "k-point", ik
 		for iin in range(nawf):
-			wfc_type=elem.findall("ATMWFC.%d"%(iin+1))[0].attrib['type']
-			aux     =elem.findall("ATMWFC.%d"%(iin+1))[0].text
+			subelem = elem.findall("ATMWFC.%d"%(iin+1))[0]
+			if verbose:print("Reading",subelem.tag, "SPIN component", ispin, "k-point", ik)
+			wfc_type=subelem.attrib['type']
+			aux     =subelem.text
                 	aux = np.array(re.split(',|\n',aux.strip()),dtype='float32')
 			if wfc_type=='real':
 			    wfc = aux.reshape((nbnds,1))#wfc = nbnds x 1
@@ -187,7 +201,6 @@ def read_QE_output_xml(fpath):
 			    U[:,iin,ik,ispin] = wfc[:,0]+1j*wfc[:,1]
 			else:
 			    sys.exit('neither real nor complex??')
-
 
             #Finish reading eigen values
             if elem.tag == "EIGENVALUES":
@@ -213,9 +226,9 @@ def read_QE_output_xml(fpath):
                     ispin = 0
 
 
-    return(U, my_eigsmat, alat, a_vectors, b_vectors, nkpnts, nspin, kpnts, kpnts_wght, nbnds, Efermi, nawf, nk1, nk2, nk3)
-
-#if __name__ == "__main__":
-#	fpath="./fe.save"
-#	read_QE_output_xml(fpath)
+    #return(U, my_eigsmat, alat, a_vectors, b_vectors, nkpnts, nspin, kpnts, \
+    #        kpnts_wght, nbnds, Efermi, nawf, nk1, nk2, nk3, natoms)
+if __name__ == "__main__":
+	fpath="./fe.save"
+	read_QE_output_xml(fpath,True,True)
 
