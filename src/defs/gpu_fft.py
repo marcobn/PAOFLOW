5f6586a8476ba7ac6c34a19a93c5d317307532dd
Frank Cerasoli <ftc0010@t3-login2.cm.cluster>
2017-06-12 10:26:41 -0500

    Implemented and tested GPU FFT

--- src/defs/gpu_fft.py
+++ src/defs/gpu_fft.py
@@ -1,3 +1,14 @@
+#
+# PAOFLOW
+#
+# Utility to construct and operate on Hamiltonians from the Projections of DFT wfc on Atomic Orbital bases (PAO)
+#
+# Copyright (C) 2016,2017 ERMES group (http://ermes.unt.edu, mbn@unt.edu)
+# This file is distributed under the terms of the
+# GNU General Public License. See the file `License'
+# in the root directory of the present distribution,
+# or http://www.gnu.org/copyleft/gpl.txt .
+#
 import numpy as np
 import sys, time
 import multiprocessing
