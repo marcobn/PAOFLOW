3d3c989b75afc90de7e0479acec1c9547c1444b6
Frank Cerasoli <ftc0010@login3.stampede.tacc.utexas.edu>
2017-07-13 21:59:54 -0500

    Added comments

--- src/defs/communication.py
+++ src/defs/communication.py
@@ -19,34 +19,40 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+# Scatters first dimension of an array of arbitrary length
 def scatter_array ( arr, auxshape, pydtype, sroot ):
-
+    # An array to store the size and dimensions of scattered arrays
     lsizes = np.empty((size,3), dtype=int)
     if rank == 0:
         lsizes = load_sizes(size, arr.shape[0], arr[0].size)
-
     comm.Bcast([lsizes, MPI.INT], root=0)
 
+    # Change the first dimension of auxshape to the correct size for scatter
     auxshape = list(auxshape)
     auxshape[0] = lsizes[rank][2]
 
+    # Initialize aux array
     arraux = np.empty(auxshape, dtype=pydtype)
 
+    # Get the datatype for the MPI transfer
     mpidtype = MPI._typedict[np.dtype(pydtype).char]
 
+    # Scatter the data according to load_sizes
     comm.Scatterv([arr, lsizes[:,0], lsizes[:,1], mpidtype], [arraux, mpidtype], root=sroot)
 
     return arraux
 
-
+# Scatters first dimension of an array of arbitrary length
 def gather_array ( arr, arraux, pydtype, sroot ):
-
+    # An array to store the size and dimensions of gathered arrays
     lsizes = np.empty((size,3), dtype=int)
     if rank == 0:
         lsizes = load_sizes(size, arr.shape[0], arr[0].size)
-
     comm.Bcast([lsizes, MPI.INT], root=0)
 
+    # Get the datatype for the MPI transfer
     mpidtype = MPI._typedict[np.dtype(pydtype).char]
 
+
+    # Gather the data according to load_sizes
     comm.Gatherv([arraux, mpidtype], [arr, lsizes[:,0], lsizes[:,1], mpidtype], root=sroot)
