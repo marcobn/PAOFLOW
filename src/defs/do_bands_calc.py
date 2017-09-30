179ddaa19075829cd3884770132211a650c74a1a
Andrew Supka <supka1ar@cmich.edu>
2017-09-30 08:04:28 -0400

    more memory efficient parallel bands

--- src/defs/do_bands_calc.py
+++ src/defs/do_bands_calc.py
@@ -11,9 +11,12 @@
 #
 from scipy import fftpack as FFT
 import numpy as np
-import psutil
+try:
+    import psutil
+except: pass
+
 import cmath
-import sys
+import sys,os
 
 from mpi4py import MPI
 from mpi4py.MPI import ANY_SOURCE
@@ -32,84 +35,105 @@ comm=MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+
+import scipy.linalg as LAN
+
 def do_bands_calc(HRaux,SRaux,kq,R_wght,R,idx,read_S,inputpath):
-    # Compute bands on a selected path in the BZ
-
     # Load balancing
-    nkpi=kq.shape[1]
-    ini_ik, end_ik = load_balancing(size,rank,nkpi)
-    nsize = end_ik - ini_ik
+    nawf,nawf,nk1,nk2,nk3,nspin = HRaux.shape    
+    
+    ini_ik, end_ik = load_balancing(size,rank,kq.shape[1]) 
 
-    nawf,nawf,nk1,nk2,nk3,nspin = HRaux.shape
-    if rank == 0:
-        Hks_int  = np.zeros((nkpi,nawf,nawf,nspin),dtype=complex) # final data arrays
+
+    if read_S:
+        Sks_aux = band_loop_S(ini_ik,end_ik,nspin,nk1,nk2,nk3,nawf,SRaux,R_wght,kq,R,idx)
+    else: Sks_aux = None
+
+    Hks_aux = band_loop_H(ini_ik,end_ik,nspin,nk1,nk2,nk3,nawf,HRaux,R_wght,kq,R,idx)
+
+    E_kp_aux = np.zeros(((end_ik-ini_ik),nawf,nspin),dtype=float,order="C")
+    v_kp_aux = np.zeros(((end_ik-ini_ik),nawf,nawf,nspin),dtype=complex,order="C")
+
+    for ispin in xrange(nspin):
+        for ik in xrange(end_ik-ini_ik):
+            if read_S:
+                E_kp_aux[ik,:,ispin],v_kp_aux[ik,:,:,ispin] = LAN.eigh(Hks_aux[:,:,ik,ispin], 
+                                                                       b=Sks_aux[:,:,ik],lower=False, 
+                                                                       overwrite_a=True,
+                                                                       overwrite_b=True,
+                                                                       turbo=True,check_finite=True)
+            else:   
+                E_kp_aux[ik,:,ispin],v_kp_aux[ik,:,:,ispin] = LAN.eigh(Hks_aux[:,:,ik,ispin], 
+                                                                       b=None,lower=False, 
+                                                                       overwrite_a=True,
+                                                                       overwrite_b=True,
+                                                                       turbo=True,check_finite=True)
+                 
+    Hks_aux = None
+    Sks_aux = None
+
+    if rank==0:
+        E_kp = np.zeros((kq.shape[1],nawf,nspin),order="C")
+        gather_array(E_kp,E_kp_aux)
     else:
-        Hks_int = None
+        gather_array(None,E_kp_aux)
 
-    # Band loop on Hks
-    Hks_aux  = np.zeros((nawf,nawf,nsize,nspin),dtype=complex) # read data arrays from tasks
-    Hks_aux[:,:,:,:] = band_loop_H(ini_ik,end_ik,nspin,nk1,nk2,nk3,nawf,HRaux,R_wght,kq,R,idx)
-
+    vecs=False
     comm.Barrier()
 
-    # Gather segments of Hks
-    Hks_aux = np.ascontiguousarray(np.moveaxis(Hks_aux, 2, 0))
-    gather_array(Hks_int, Hks_aux)
-    if rank == 0:
-        Hks_int = np.moveaxis(Hks_int, 0, 2)
+    if vecs:
+        if rank==0:
+            v_kp = np.zeros((kq.shape[1],nawf,nawf,nspin),order="C")
+            gather_array(v_kp,v_kp_aux)
+        else:
+            gather_array(None,v_kp_aux)
 
-    Sks_int  = np.zeros((nkpi,nawf,nawf),dtype=complex)
-    if read_S:
-        # Band loop on Sks
-        Sks_aux  = np.zeros((nawf,nawf,nsize,1),dtype=complex)
-        Sks_aux[:,:,:,0] = band_loop_S(ini_ik,end_ik,nspin,nk1,nk2,nk3,nawf,SRaux,R_wght,kq,R,idx)
-
-        comm.Barrier()
-
-        # Gather segments of Sks
-        Sks_aux = np.ascontiguousarray(np.moveaxis(Sks_aux, 2, 0))
-        gather_array(Sks_int, Sks_aux)
-        if rank == 0:
-            Sks_int = np.moveaxis(Sks_int, 0, 2)
-
-    E_kp = np.zeros((nkpi,nawf,nspin),dtype=float)
-    v_kp = None
-    evecs = False
-    if nkpi*nawf*nawf*nspin*size*16. < psutil.virtual_memory().total/2.:
-        v_kp = np.zeros((nkpi,nawf,nawf,nspin),dtype=complex)
-        evecs = True
-
-    if rank == 0:
+    else:
+        v_kp = None
+    v_kp_aux = None
+    E_kp_aux = None
+  
+    if rank==0:
         for ispin in xrange(nspin):
-            if evecs:
-                E_kp[:,:,ispin],v_kp[:,:,:,ispin] = write_PAO_eigs(Hks_int,Sks_int,read_S,ispin,evecs,inputpath)
-            else:
-                E_kp[:,:,ispin],v_kp = write_PAO_eigs(Hks_int,Sks_int,read_S,ispin,evecs,inputpath)
+            f=open(inputpath+'/bands_'+str(ispin)+'.dat','w')
+            for ik in xrange(kq.shape[1]):
+                s="%d\t"%ik
+                for  j in E_kp[ik,:,ispin]:s += "% 3.5f\t"%j
+                s+="\n"
+                f.write(s)
+            f.close()
+            
+        return E_kp,v_kp
+    else: return None,None
 
-    comm.Bcast(E_kp,root=0)
-    if evecs:
-        comm.Bcast(v_kp,root=0)
-
-#    if rank == 0:
-#        plt.matshow(abs(Hks_int[:,:,1445,0]))
-#        plt.colorbar()
-#        plt.show()
-#
-#        np.save('Hks_noSO0',Hks_int[:,:,0,0])
-
-    return(E_kp,v_kp)
 
 def band_loop_H(ini_ik,end_ik,nspin,nk1,nk2,nk3,nawf,HRaux,R_wght,kq,R,idx):
 
-    nsize = end_ik - ini_ik
-    auxh = np.zeros((nawf,nawf,nsize,nspin),dtype=complex)
     HRaux = np.reshape(HRaux,(nawf,nawf,nk1*nk2*nk3,nspin),order='C')
+    kdot = np.zeros(((end_ik-ini_ik),R.shape[0]),dtype=complex,order="C")
+    kdot = np.tensordot(R,2.0j*np.pi*kq[:,ini_ik:end_ik],axes=([1],[0]))
+    np.exp(kdot,kdot)
 
-    for ik in xrange(ini_ik,end_ik):
-        for ispin in xrange(nspin):
-             auxh[:,:,ik-ini_ik,ispin] = np.sum(HRaux[:,:,:,ispin]*np.exp(2.0*np.pi*kq[:,ik].dot(R[:,:].T)*1j),axis=2)
+    auxh = np.zeros((nawf,nawf,(end_ik-ini_ik),nspin),dtype=complex,order="C")
 
-    return(auxh)
+    for ispin in xrange(nspin):
+        auxh[:,:,:,ispin]=np.tensordot(HRaux[:,:,:,ispin],kdot,axes=([2],[0]))
+
+    kdot  = None
+    return auxh
+
+
+# def band_loop_H(ini_ik,end_ik,nspin,nk1,nk2,nk3,nawf,HRaux,R_wght,kq,R,idx):
+
+#     nsize = end_ik - ini_ik
+#     auxh = np.zeros((nawf,nawf,nsize,nspin),dtype=complex)
+#     HRaux = np.reshape(HRaux,(nawf,nawf,nk1*nk2*nk3,nspin),order='C')
+
+#     for ik in xrange(ini_ik,end_ik):
+#         for ispin in xrange(nspin):
+#              auxh[:,:,ik-ini_ik,ispin] = np.sum(HRaux[:,:,:,ispin]*np.exp(2.0*np.pi*kq[:,ik].dot(R[:,:].T)*1j),axis=2)
+
+#     return(auxh)
 
 def band_loop_S(ini_ik,end_ik,nspin,nk1,nk2,nk3,nawf,SRaux,R_wght,kq,R,idx):
 
