ec4654d8865b8492f2c601843878522027c30bdb
Andrew Supka <supka1ar@cmich.edu>
2017-10-01 13:18:27 -0400

    fix in full parallel do_adaptive_smearing

--- src/defs/do_adaptive_smearing.py
+++ src/defs/do_adaptive_smearing.py
@@ -20,49 +20,39 @@ from load_balancing import *
 
 def do_adaptive_smearing(pksp,nawf,nspin,alat,a_vectors,nk1,nk2,nk3,smearing):
 
-    if smearing != None:
-        #----------------------
-        # adaptive smearing as in Yates et al. Phys. Rev. B 75, 195121 (2007).
-        #----------------------
+    if smearing == None:
+        return
+
+    #----------------------
+    # adaptive smearing as in Yates et al. Phys. Rev. B 75, 195121 (2007).
+    #----------------------
 
 
-        deltakp = np.zeros((pksp.shape[0],nawf,nspin),dtype=float)
+    deltakp = np.zeros((pksp.shape[0],nawf,nspin),dtype=float)
 
-        diag = np.diag_indices(nawf)
+    diag = np.diag_indices(nawf)
 
+    omega = alat**3 * np.dot(a_vectors[0,:],np.cross(a_vectors[1,:],a_vectors[2,:]))
+    dk = (8.*np.pi**3/omega/(nk1*nk2*nk3))**(1./3.)
 
+    if smearing == 'gauss':
+        afac = 0.7
+    elif smearing == 'm-p':
+        afac = 1.0        
 
-        omega = alat**3 * np.dot(a_vectors[0,:],np.cross(a_vectors[1,:],a_vectors[2,:]))
-        dk = (8.*np.pi**3/omega/(nk1*nk2*nk3))**(1./3.)
+    pksaux= pksp[:,:,diag[0],diag[1]]
 
-        if smearing == 'gauss':
-            afac = 0.7
-        elif smearing == 'm-p':
-            afac = 1.0
-        
+    deltakp = np.zeros((pksaux.shape[0],nawf,nspin),dtype=float)
+    np.power(np.sum(np.power(np.real(pksaux),2.0,order="C"),axis=1),0.5,order="C",out=deltakp)
 
+    deltakp2aux = np.zeros((pksaux.shape[0],3,nawf,nawf,nspin),dtype=float)
+    for ispin in xrange(pksaux.shape[3]):
+        for n in xrange(nawf):
+                for m in xrange(nawf):
+                    deltakp2aux[:,:,n,m,:] = np.real(np.absolute(pksaux[:,:,n,:]-pksaux[:,:,m,:]))
 
-
-
-        pksaux= pksp[:,:,diag[0],diag[1]]
-
-        deltakp = np.zeros((pksaux.shape[0],nawf,nspin),dtype=float)
-        np.power(np.sum(np.power(np.real(pksaux),2.0,order="C"),axis=1),0.5,order="C",out=deltakp)
-
-        deltakp2aux = np.zeros((pksaux.shape[0],3,nawf,nawf,nspin),dtype=float)
-        for ispin in xrange(pksaux.shape[3]):
-            for n in xrange(nawf):
-                    for m in xrange(nawf):
-                        deltakp2aux[:,:,n,m,:] = np.real(np.absolute(pksaux[:,:,n,:]-pksaux[:,:,m,:]))
-
-        deltakp2 = np.zeros((pksp.shape[0],nawf,nawf,nspin),dtype=float)
-        deltakp2 = np.power(np.sum(np.power(deltakp2aux,2.0,order="C"),axis=1),0.5,order="C") 
-
-
-
-
-
-
+    deltakp2 = np.zeros((pksp.shape[0],nawf,nawf,nspin),dtype=float)
+    deltakp2 = np.power(np.sum(np.power(deltakp2aux,2.0,order="C"),axis=1),0.5,order="C") 
 
     deltakp*=afac*dk
     deltakp2*=afac*dk
