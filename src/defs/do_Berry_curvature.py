f2df4b6102f0c66731aebfd79900a753606e5dcc
Andrew Supka <supka1ar@cmich.edu>
2017-10-01 14:25:30 -0400

    initial commit for do_berry_curvature.py full parallel vecs

--- src/defs/do_Berry_curvature.py
+++ src/defs/do_Berry_curvature.py
@@ -37,107 +37,33 @@ def do_Berry_curvature(E_k,pksp,nk1,nk2,nk3,npool,ipol,jpol,eminSH,emaxSH,fermi_
     # Compute spin Berry curvature
     #----------------------
 
-    index = None
+    _,_,nawf,nawf,nspin = pksp.shape
 
-    if rank == 0:
-        nktot,_,nawf,nawf,nspin = pksp.shape
-        index = {'nktot':nktot,'nawf':nawf,'nspin':nspin}
+    Om_znk = np.zeros((pksp.shape[0],nawf),dtype=float)
 
-    index = comm.bcast(index,root=0)
+    deltap = 0.05
 
-    nktot = index['nktot']
-    nawf = index['nawf']
-    nspin = index['nspin']
-
-    # Compute only Omega_z(k)
-
-    if rank == 0:
-        Om_znk = np.zeros((nk1*nk2*nk3,nawf),dtype=float)
-    else:
-        Om_znk = None
-
-    for pool in xrange(npool):
-        ini_ip, end_ip = load_balancing(npool, pool, nktot)
-        nkpool = end_ip - ini_ip
-
-        if rank == 0:
-            pksp_long = pksp[ini_ip:end_ip]
-            E_k_long= E_k[ini_ip:end_ip]
-            Om_znk_split = Om_znk[ini_ip:end_ip]
-        else:
-            Om_znk_split = None
-            pksp_long = None
-            E_k_long = None
-
-        pksaux = scatter_array(pksp_long)
-        E_kaux = scatter_array(E_k_long)
-
-        # Load balancing
-        ini_ik, end_ik = load_balancing(size,rank,nkpool)
-        nsize = end_ik-ini_ik
-
-        Om_znkaux = np.zeros((nsize,nawf),dtype=float)
-
-        deltap = 0.05
-        for n in xrange(nawf):
-            for m in xrange(nawf):
-                if m!= n:
-                    Om_znkaux[:,n] += -2.0*np.imag(pksaux[:,ipol,n,m,0]*pksaux[:,jpol,m,n,0]) / \
-                    ((E_kaux[:,m,0] - E_kaux[:,n,0])**2 + deltap**2)
-
-        gather_array(Om_znk_split, Om_znkaux)
-
-        if rank == 0:
-            Om_znk[ini_ip:end_ip,:] = Om_znk_split[:,:]
+    for n in xrange(nawf):
+        for m in xrange(nawf):
+            if m!= n:
+                Om_znk[:,n] += -2.0*np.imag(pksp[:,ipol,n,m,0]*pksp[:,jpol,m,n,0]) / \
+                ((E_k[:,m,0] - E_k[:,n,0])**2 + deltap**2)
 
     de = (emaxSH-eminSH)/500
     ene = np.arange(eminSH,emaxSH,de,dtype=float)
 
-    if rank == 0:
-        Om_zk = np.zeros((nk1*nk2*nk3,ene.size),dtype=float)
-    else:
-        Om_zk = None
+    Om_zk = np.zeros((pksp.shape[0],ene.size),dtype=float)
 
-    pksp_long = None
-    E_k_long = None
-
-    for pool in xrange(npool):
-        ini_ip, end_ip = load_balancing(npool, pool, nktot)
-        nkpool = end_ip - ini_ip
-
-        if rank == 0:
-            E_k_long = E_k[ini_ip:end_ip]
-            Om_znk_long = Om_znk[ini_ip:end_ip]
-            Om_zk_split = Om_zk[ini_ip:end_ip]
-            deltak_long= deltak[ini_ip:end_ip]
+    for i in xrange(ene.size):
+        if smearing == 'gauss':
+            Om_zk[:,i] = np.sum(Om_znk[:,:]*intgaussian(E_k[:,:,0],ene[i],deltak[:,:,0]),axis=1)
+        elif smearing == 'm-p':
+            Om_zk[:,i] = np.sum(Om_znk[:,:]*intmetpax(E_k[:,:,0],ene[i],deltak[:,:,0]),axis=1)
         else:
-            Om_znk_long = None
-            Om_zk_split = None
-            E_k_long = None
-            deltak_long = None
+            Om_zk[:,i] = np.sum(Om_znk[:,:]*(0.5 * (-np.sign(E_k[:,:,0]-ene[i]) + 1)),axis=1)
 
-        # Load balancing
-        ini_ik, end_ik = load_balancing(size,rank,nkpool)
-        nsize = end_ik-ini_ik
+    Om_zk = gather_full(Om_zk,npool)
 
-        Om_zkaux = np.zeros((nsize,ene.size),dtype=float)
-
-        Om_znkaux = scatter_array(Om_znk_long)
-        E_kaux = scatter_array(E_k_long)
-        deltakaux = scatter_array(deltak_long)
-
-        for i in xrange(ene.size):
-            if smearing == 'gauss':
-                Om_zkaux[:,i] = np.sum(Om_znkaux[:,:]*intgaussian(E_kaux[:,:,0],ene[i],deltakaux[:,:,0]),axis=1)
-            elif smearing == 'm-p':
-                Om_zkaux[:,i] = np.sum(Om_znkaux[:,:]*intmetpax(E_kaux[:,:,0],ene[i],deltakaux[:,:,0]),axis=1)
-            else:
-                Om_zkaux[:,i] = np.sum(Om_znkaux[:,:]*(0.5 * (-np.sign(E_kaux[:,:,0]-ene[i]) + 1)),axis=1)
-
-        gather_array(Om_zk_split, Om_zkaux)
-
-        if rank == 0:
-            Om_zk[ini_ip:end_ip,:] = Om_zk_split[:,:]
 
     ahc = None
     if rank == 0: ahc = np.sum(Om_zk,axis=0)/float(nk1*nk2*nk3)
