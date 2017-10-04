d30c1cee9815d918542deeb98de045535468eb95
Andrew Supka <supka1ar@cmich.edu>
2017-10-04 02:24:24 -0400

    do upper triangle of boltz tensors when smearing==None

--- src/defs/do_Boltz_tensors.py
+++ src/defs/do_Boltz_tensors.py
@@ -42,12 +42,14 @@ def do_Boltz_tensors(E_k,velkp,kq_wght,temp,ispin,deltak,smearing,t_tensor):
     L0 = np.zeros((3,3,ene.size),dtype=float)
     L0aux = np.zeros((3,3,ene.size),dtype=float)
 
+    if smearing == None:
+        t_tensor = np.array([[0,0],[1,1],[2,2],[0,1],[0,2],[1,2]],dtype=int)
+
     L0aux[:,:,:] = L_loop(ene,E_k,velkp,kq_wght,temp,ispin,0,deltak,smearing,t_tensor)
 
     comm.Reduce(L0aux,L0,op=MPI.SUM)
 
     if smearing == None:
-
         L1 = np.zeros((3,3,ene.size),dtype=float)
         L1aux = np.zeros((3,3,ene.size),dtype=float)
 
@@ -62,6 +64,18 @@ def do_Boltz_tensors(E_k,velkp,kq_wght,temp,ispin,deltak,smearing,t_tensor):
 
         comm.Reduce(L2aux,L2,op=MPI.SUM)
 
+        L0[1,0] = L0[0,1]
+        L0[2,0] = L0[2,0]
+        L0[2,1] = L0[1,2]
+
+        L1[1,0] = L1[0,1]
+        L1[2,0] = L1[2,0]
+        L1[2,1] = L1[1,2]
+
+        L2[1,0] = L2[0,1]
+        L2[2,0] = L2[2,0]
+        L2[2,1] = L2[1,2]
+
         return(ene,L0,L1,L2)
     else:
         return(ene,L0)
@@ -72,28 +86,44 @@ def L_loop(ene,E_k,velkp,kq_wght,temp,ispin,alpha,deltak,smearing,t_tensor):
 
     L = np.zeros((3,3,ene.size),dtype=float)
 
-    for n in xrange(velkp.shape[2]):
-        Eaux = (E_k[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T - ene
-        for l in xrange(t_tensor.shape[0]):
-            i = t_tensor[l][0]
-            j = t_tensor[l][1]
-            if smearing == None:
-                L[i,j,:] += np.sum((1.0/temp * kq_wght[0]*velkp[:,i,n,ispin]*velkp[:,j,n,ispin] * \
-                            1.0/2.0 * (1.0/(1.0+np.cosh(Eaux[:,:]/temp)) * np.power(Eaux[:,:],alpha)).T),axis=1)
-            if smearing == 'gauss':
-                eig = (E_k[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
-                om = ((ene*np.ones((E_k.shape[0],ene.size),dtype=float)).T).T
-                delk = (deltak[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
-                L[i,j,:] += np.sum((kq_wght[0]*velkp[:,i,n,ispin]*velkp[:,j,n,ispin] * \
-                            (gaussian(eig,om,delk) * np.power(eig-om,alpha)).T),axis=1)
 
-            if smearing == 'm-p': 
-                eig = (E_k[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
-                om = ((ene*np.ones((E_k.shape[0],ene.size),dtype=float)).T).T
-                delk = (deltak[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
+
+    if smearing == None:
+        for n in xrange(velkp.shape[2]):
+            Eaux = (E_k[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T - ene
+            for l in xrange(t_tensor.shape[0]):
+                i = t_tensor[l][0]
+                j = t_tensor[l][1]
+                if smearing == None:
+                    L[i,j,:] += np.sum((1.0/temp * kq_wght[0]*velkp[:,i,n,ispin]*velkp[:,j,n,ispin] * \
+                                1.0/2.0 * (1.0/(1.0+np.cosh(Eaux[:,:]/temp)) * np.power(Eaux[:,:],alpha)).T),axis=1)
+
+
+    if smearing == 'gauss':
+        om = ((ene*np.ones((E_k.shape[0],ene.size),dtype=float)).T).T
+        for n in xrange(velkp.shape[2]):
+            eig = (E_k[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
+            delk = (deltak[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
+            for l in xrange(t_tensor.shape[0]):
+                i = t_tensor[l][0]
+                j = t_tensor[l][1]
                 L[i,j,:] += np.sum((kq_wght[0]*velkp[:,i,n,ispin]*velkp[:,j,n,ispin] * \
-                                (metpax(eig,om,delk) * np.power(eig-om,alpha)).T),axis=1)
-            if smearing != None and smearing != 'gauss' and smearing != 'm-p':
-                sys.exit('smearing not implemented')
+                                        (gaussian(eig,om,delk) * np.power(eig-om,alpha)).T),axis=1)
+
+
+    if smearing == 'm-p': 
+        om = ((ene*np.ones((E_k.shape[0],ene.size),dtype=float)).T).T
+        for n in xrange(velkp.shape[2]):
+            eig = (E_k[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
+            delk = (deltak[:,n,ispin]*np.ones((E_k.shape[0],ene.size),dtype=float).T).T
+            for l in xrange(t_tensor.shape[0]):
+                i = t_tensor[l][0]
+                j = t_tensor[l][1]
+                L[i,j,:] += np.sum((kq_wght[0]*velkp[:,i,n,ispin]*velkp[:,j,n,ispin] * \
+                                        (metpax(eig,om,delk) * np.power(eig-om,alpha)).T),axis=1)
+                
+
+    if smearing != None and smearing != 'gauss' and smearing != 'm-p':
+        sys.exit('smearing not implemented')
 
     return(L)
