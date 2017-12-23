90994771da672b7650ef3ffb96d3f98627ba4cdf
Marco Buongiorno Nardelli <marco@Marcos-MBP.fios-router.home>
2017-12-23 17:56:00 -0600

    erased pyc

---
+++ src/defs/include.py
@@ -0,0 +1,17 @@
+#
+# PAOFLOW
+#
+# Utility to construct and operate on Hamiltonians from the Projections of DFT wfc on Atomic Orbital bases (PAO)
+#
+# Copyright (C) 2016,2017 ERMES group (http://ermes.unt.edu, mbn@unt.edu)
+# This file is distributed under the terms of the
+# GNU General Public License. See the file `License'
+# in the root directory of the present distribution,
+# or http://www.gnu.org/copyleft/gpl.txt .
+#
+
+import os
+
+def include(filename):
+    if os.path.exists(filename): 
+        execfile(filename)
