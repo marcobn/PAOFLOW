4fe724a2480cc1b144206544a0ac3fe5c5a1a1d9
Franklin Cerasoli <frank@mapped-129-120-30-7.dynamic.unt.edu>
2018-11-07 13:55:29 -0600

    Setup.py working, not filled out.

--- setup.py
+++ setup.py
@@ -2,25 +2,6 @@ from distutils.core import setup
 import os
 
 defs = os.path.join('src','defs')
-pys = os.listdir(defs)
-rml = []
-
-for i in range(len(pys)):
-  v = pys[i]
-  if v[0] == '_' or v[-3:] != '.py':
-    rml.append(v)
-  else:
-    pys[i] = v[:-3]
-
-for v in rml:
-  pys.remove(v)
-
-pkgs = ['PAOFLOW'] + pys
-pkgd = { 'PAOFLOW' : 'src' }
-for p in pys:
-  pkgd[p] = defs
-#print(pkgs)
-#print(pkgd)
 
 setup(name='PAOFOO',
       version='1.0',
@@ -29,5 +10,5 @@ setup(name='PAOFOO',
       author_email='mbn@unt.edu',
       platforms='Linux',
       url='ERMES',
-      packages=pkgs,
-      package_dir=pkgd)
+      packages=['PAOFLOW', 'PAOFLOW.defs'],
+      package_dir={'PAOFLOW':'src'})
