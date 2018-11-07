2ff46ae23d19da6899a0eef47ea067a2bafc1329
Franklin Cerasoli <frank@mapped-129-120-30-7.dynamic.unt.edu>
2018-11-07 13:38:28 -0600

    More setup contributions.

--- setup.py
+++ setup.py
@@ -3,16 +3,31 @@ import os
 
 defs = os.path.join('src','defs')
 pys = os.listdir(defs)
+rml = []
 
-print(pys)
+for i in range(len(pys)):
+  v = pys[i]
+  if v[0] == '_' or v[-3:] != '.py':
+    rml.append(v)
+  else:
+    pys[i] = v[:-3]
 
-#setup(name='PAOFOO',
-#      version='1.0',
-#      summary='Electronic Structure Post-processing Tools',
-#      author='Marco Buongiorno Nardelli',
-#      author_email='mbn@unt.edu',
-#      platforms='Linux',
-#      url='ERMES',
-#      packages=['PAOFLOW', 'defone'],
-#      package_dir={'PAOFLOW':'src', 'defone':'src/defs'})
-#      py_modules=['PAOFOO'])
+for v in rml:
+  pys.remove(v)
+
+pkgs = ['PAOFLOW'] + pys
+pkgd = { 'PAOFLOW' : 'src' }
+for p in pys:
+  pkgd[p] = defs
+#print(pkgs)
+#print(pkgd)
+
+setup(name='PAOFOO',
+      version='1.0',
+      summary='Electronic Structure Post-processing Tools',
+      author='Marco Buongiorno Nardelli',
+      author_email='mbn@unt.edu',
+      platforms='Linux',
+      url='ERMES',
+      packages=pkgs,
+      package_dir=pkgd)
