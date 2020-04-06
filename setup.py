af4b1b6fea0392b4c528f4397018848e1c62551b
Andrew Supka <supka1ar@cmich.edu>
2020-04-06 03:05:40 -0400

    update setup.py with reqs

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
+import setuptools
 from distutils.core import setup
 import os
 
 defs = os.path.join('src','defs')
 
 setup(name='PAOFLOW',
-      version='1.0',
+      version='1.0.2',
       description='Electronic Structure Post-processing Tools',
       author='Marco Buongiorno Nardelli',
       author_email='mbn@unt.edu',
       platforms='Linux',
-      url='ERMES',
+      url='http://aflowlib.org/src/paoflow/',
       packages=['PAOFLOW', 'PAOFLOW.defs'],
-      package_dir={'PAOFLOW':'src'})
+      package_dir={'PAOFLOW':'src'},
+      install_requires=["numpy","scipy","mpi4py","z2pack","tbmodels"])
