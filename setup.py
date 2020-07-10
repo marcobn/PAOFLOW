9a5989d327d0ebf64d097733807a4b19053cf073
Frank Cerasoli <franklin.cerasoli@unt.edu>
2020-07-10 11:21:42 -0500

    Changed name from paoflow to PAOFLOW

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 defs = os.path.join('src','defs')
 
-setup(name='paoflow',
+setup(name='PAOFLOW',
       version='2.0.1',
       description='Electronic Structure Post-processing Tools',
       author='Marco Buongiorno Nardelli',
