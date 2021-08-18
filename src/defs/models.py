e3f809f07baf31e998798c0e1ff46cac2dbd360a
Bruno Focassio <bfocassio@gmail.com>
2021-08-18 17:48:14 -0300

    Implemented graphene2 model with staggered onsite term

--- src/defs/models.py
+++ src/defs/models.py
@@ -223,6 +223,69 @@ def graphene( data_controller, params ):
 
   arry['atoms']=['C','C']
 
+def graphene2( data_controller, params ):
+  from .constants import ANGSTROM_AU
+  from scipy.fftpack import fftshift
+  from mpi4py import MPI
+  import numpy as np
+
+  arry,attr = data_controller.data_dicts()
+
+  attr['nk1'] = 3
+  attr['nk2'] = 3
+  attr['nk3'] = 1
+
+  attr['nawf'] = 2
+  attr['nspin'] = 1
+  attr['natoms'] = 2
+
+  arry['naw'] = np.array([1,1])
+
+  attr['alat'] = 2.46 * ANGSTROM_AU
+
+  arry['HRs'] = np.zeros((attr['nawf'],attr['nawf'],attr['nk1'],attr['nk2'],attr['nk3'],attr['nspin']),dtype=complex)
+
+  # H00
+  arry['HRs'][0,0,0,0,0,0] = params['delta']/2
+  arry['HRs'][1,1,0,0,0,0] = -params['delta']/2
+
+  # H00
+  arry['HRs'][0,1,0,0,0,0] = params['t']
+  arry['HRs'][1,0,0,0,0,0] = params['t']
+
+  # H10
+  arry['HRs'][1,0,1,0,0,0] = params['t']
+
+  #H20
+  arry['HRs'][:,:,2,0,0,0] = np.conj(arry['HRs'][:,:,1,0,0,0]).T
+
+  #H01
+  arry['HRs'][1,0,0,1,0,0] = params['t']
+
+  #H02
+  arry['HRs'][:,:,0,2,0,0] = np.conj(arry['HRs'][:,:,0,1,0,0]).T
+
+  # Lattice Vectors
+  arry['a_vectors'] = np.zeros((3,3),dtype=float)
+  arry['a_vectors'] = np.array([[1., 0, 0], [0.5, 3 ** .5 / 2, 0], [0, 0, 10]])
+  arry['a_vectors'] = arry['a_vectors']
+
+  # Atomic coordinates
+  arry['tau'] = np.zeros((2,3),dtype=float) 
+
+  arry['tau'][0,0] = 0.50000 ;  arry['tau'][0,1] = 0.28867
+  arry['tau'][1,0] = 1.00000 ;  arry['tau'][1,1] = 0.57735
+
+
+  # Reciprocal Lattice
+  arry['b_vectors'] = np.zeros((3,3),dtype=float)
+  volume = np.dot(np.cross(arry['a_vectors'][0,:],arry['a_vectors'][1,:]),arry['a_vectors'][2,:])
+  arry['b_vectors'][0,:] = (np.cross(arry['a_vectors'][1,:],arry['a_vectors'][2,:]))/volume
+  arry['b_vectors'][1,:] = (np.cross(arry['a_vectors'][2,:],arry['a_vectors'][0,:]))/volume
+  arry['b_vectors'][2,:] = (np.cross(arry['a_vectors'][0,:],arry['a_vectors'][1,:]))/volume 
+
+  arry['atoms']=['C','C']
+
 
 def cubium( data_controller, params ):
   from .constants import ANGSTROM_AU
@@ -441,6 +504,8 @@ def Kane_Mele( data_controller, params ):
 def build_TB_model ( data_controller, parameters ):
   if parameters['label'].upper() == 'GRAPHENE':
     graphene(data_controller, parameters)
+  elif parameters['label'].upper() == 'GRAPHENE2':
+    graphene2(data_controller, parameters)
   elif parameters['label'].upper() == 'CUBIUM':
     cubium(data_controller, parameters)
   elif parameters['label'].upper() == 'CUBIUM2':
