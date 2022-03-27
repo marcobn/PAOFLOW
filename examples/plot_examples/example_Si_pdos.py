ff6e3ec6d717f0d0cf97e60db78c36a2c8c3ba49
Frank Cerasoli <ftcerasoli@ucdavis.edu>
2022-03-26 22:28:36 -0700

    Sum over atoms in addition to orbital levels

--- examples/plot_examples/example_Si_pdos.py
+++ examples/plot_examples/example_Si_pdos.py
@@ -11,28 +11,29 @@ if __name__ == '__main__':
   # Display a default plot of all pdos files
   pplt.plot_pdos(fnames)
 
-  # Create labels and colros, to override default indices and color wheel
+  # Create a table indicating how the pdos should be summed prior to plotting
+  #  A list for s, p and d
   ind = 0
-  cols = []
-  labels = []
-  # Also create a table indicating how the pdos should be summed prior to plotting
-  psums = []
+  psums = [[], [], []]
+
+  # Create labels and colros to override defaults
+  labels = ['s', 'p', 'd']
+  cols = [[1,0,0], [.8,.2,0], [.6,.4,0]]
 
   # Iterate over the shell levels for each shell of each atom
+  #   appending the pdos index to appropriate shell array
   for i,atom in enumerate(['Si', 'Si']):
-    for j,orbital in enumerate(['s', 'p', 'd']):
-      sum_inds = []
+    for j,orbital in enumerate(labels):
       for _ in range(2*j+1):
-        sum_inds.append(ind)
+        psums[j].append(ind)
         ind += 1
-      psums.append(sum_inds)
-      cols.append([i/2+.5, j/3+.2, 0])
-      labels.append('{}{}_{}'.format(atom,i,orbital))
 
   # Add the elements for summing total dos
   psums.append(list(range(ind)))
   cols.append([0,0,0])
   labels.append('Total')
 
+
   # Plot each shell for each atom
   pplt.plot_pdos(fnames, cols=cols, labels=labels, psum_inds=psums)
+
