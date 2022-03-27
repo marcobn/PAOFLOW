6fafb1793db26fdbe0edd5c90efc4dc52c269d53
Frank Cerasoli <ftcerasoli@ucdavis.edu>
2022-03-26 21:59:48 -0700

    Fixed title typo in example

--- examples/plot_examples/example_Al_conductivity.py
+++ examples/plot_examples/example_Al_conductivity.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
 
   # Argument t_ele default is [[0,0], [1,1], [2,2]]
   #   which plots the 3 diagonal elements
-  pplt.plot_electrical_conductivity(f_sigma, title='Ba8Cu16As30 Conductivity')
+  pplt.plot_electrical_conductivity(f_sigma, title='Aluminum Conductivity')
 
