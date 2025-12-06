fa4804ece6d86dadd2f9da5be5c7fa44223e62b0
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-12-06 17:38:37 +0100

    Ensure wavefunction overlaps are saved when using ACBN0

--- src/PAOFLOW/PAOFLOW.py
+++ src/PAOFLOW/PAOFLOW.py
@@ -423,6 +423,11 @@ class PAOFLOW:
     #  Note expand_wedge is still required for VASP even not using symmetry.
     #  This is because we need find_equiv_k() in paosym to have the correct k-point ordering.
 
+    if attr['acbn0'] and not attr['save_overlaps']:
+      if self.rank == 0:
+        print('WARNING: ACBN0 requires wavefunction overlaps. Setting save_overlaps to True.')
+      attr['save_overlaps'] = True
+
     if attr['symmetrize'] and attr['acbn0']:
       if rank == 0:
         print('WARNING: Non-ortho is currently not supported with pao_sym. Use nosym=.true., noinv=.true.')
