8c93cd4ba8196a181559e2a036be2e8bd67870c3
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-01-15 09:30:26 +0100

    Add transport calculation to `main.py`

--- examples/transport_examples/example01/main_conductor.py
+++ examples/transport_examples/example01/main_conductor.py
@@ -1,8 +1,9 @@
-import yaml
-
 from PAOFLOW import PAOFLOW
-from PAOFLOW.transport.parsers.parser_base import parse_args
 from PAOFLOW.transport.Transport import ConductorRunner
+from mpi4py import MPI
+
+
+comm = MPI.COMM_WORLD
 
 
 def main():
@@ -12,18 +13,21 @@ def main():
         smearing="gauss",
         npool=1,
         verbose=True,
+        save_overlaps=True,
     )
 
     paoflow.read_atomic_proj_QE()
-    arry, attr = paoflow.data_controller.data_dicts()
     paoflow.projectability()
-    paoflow.pao_hamiltonian()
-    print(arry.keys())
-    print(attr.keys())
-    print(arry["Hks"][:, :, 0, 0, 0, 0])
-    yaml_file = parse_args()
-    # print(arry["HRs"].shape)
-    #### calls to the transportPAO modules here
+    paoflow.pao_hamiltonian(shift_type=1)
+    paoflow.projections()
+
+    data_controller = paoflow.data_controller
+
+    transport = ConductorRunner.from_yaml(
+        yaml_file="conductor.yaml",
+        data_controller=data_controller,
+    )
+    transport.run()
 
 
 if __name__ == "__main__":
