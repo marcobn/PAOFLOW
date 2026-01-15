fce0656568726492d1e9ca5322e871ccb8476fa8
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-01-15 09:30:26 +0100

    Use PAO kspace Hamiltonian

--- src/PAOFLOW/transport/io/input_parameters.py
+++ src/PAOFLOW/transport/io/input_parameters.py
@@ -124,10 +124,6 @@ class IterationConvergenceSettings(PydanticBaseModel):
 
 class AtomicProjectionOverlapSettings(PydanticBaseModel):
     do_overlap_transformation: bool = False
-    atmproj_sh: NonNegativeFloat = 5.0
-    atmproj_thr: Annotated[NonNegativeFloat, confloat(ge=0.0, le=1.0)] = 0.9
-    atmproj_nbnd: NonNegativeInt = 0
-    atmproj_do_norm: bool = False
     # TODO : check if the normalization option is still needed and is responsible for different results
     write_intermediate: bool = True
 
