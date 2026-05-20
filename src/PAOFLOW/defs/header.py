b45c931b5cec015ca5429dca191d2fbe668c7690
Marco Buongiorno Nardelli <mbn@unt.edu>
2026-05-20 10:08:06 -0500

    new header

--- src/PAOFLOW/defs/header.py
+++ src/PAOFLOW/defs/header.py
@@ -1,32 +1,59 @@
-def header():
-    """Print the PAOFLOW ASCII banner to standard output.
+# def header():
+#     """Print the PAOFLOW ASCII banner to standard output.
 
-    Returns
-    -------
-    None
+#     Returns
+#     -------
+#     None
+#     """
+#     print('')
+#     print(
+#         '#############################################################################################'
+#     )
+#     print(
+#         '#                                                                                           #'
+#     )
+#     print(
+#         '#                                          PAOFLOW                                          #'
+#     )
+#     print(
+#         '#                                                                                           #'
+#     )
+#     print(
+#         '#                  Utility to construct and operate on Hamiltonians from                    #'
+#     )
+#     print(
+#         '#                 the Projections of DFT wfc on Atomic Orbital bases (PAO)                  #'
+#     )
+#     print(
+#         '#                                                                                           #'
+#     )
+#     print(
+#         '#############################################################################################\n'
+#     )
+def header(style='large'):
+    """Print the PAOFLOW logo.
+
+    Parameters
+    ----------
+    style : str
+        'large' for the full letter-art logo (default), 'small' for the
+        compact self-filled logo.
     """
-    print('')
-    print(
-        '#############################################################################################'
+    large = (
+        'PPPPPP\\   AA\\AAAAA\\   OOOOOO\\  FFFFFFFF\\ LLL\\        OOOOOO\\  WW\\   WW\\   WW\\ \n'
+        'PP  __PP\\ AA  __$$\\  OO  __OO\\ FF  ____/ L  |       OO  __OO\\ W  |  W  |  W  |\n'
+        'PP /  P  |AA /  @  | OO /  O  |FF /      L  |       OO /  O  |W  |  W  |  W  |\n'
+        'PPPPPPP  |AAAAAAAA | OO |  O  |FFFFFF    L  |       OO |  O  |W  |  W  |  W  |\n'
+        'PP  ____/ AA  __AA | OO |  O  |FF  _/    L  |       OO |  O  |W |  W |  W | \n'
+        'PP /      AA /  A  | OO \\__O  |FF /      L  |       OO \\__O  |W |  W |  W | \n'
+        'PP/       AA/   A  |  \\OOOOOO  |FF/       LLLLLLLL\\  \\OOOOOO  |\\WWWWWWWWWW /  \n'
+        '\\__|      \\__|  \\__|   \\______/ \\__|      \\________|  \\______/  \\_________/   \n'
     )
-    print(
-        '#                                                                                           #'
+    small = (
+        'ppppp     aa     oooooo  ffff  l     oooooo  ww        ww\n'
+        'p  pp    aaaa    oo  oo  ffff  l     oo  oo   ww  ww  ww\n'
+        'ppppp   aaaaaa   oo  oo  f     l     oo  oo    wwwwwwww\n'
+        'p      aa    aa  oooooo  f     llll  oooooo     ww  ww\n'
     )
-    print(
-        '#                                          PAOFLOW                                          #'
-    )
-    print(
-        '#                                                                                           #'
-    )
-    print(
-        '#                  Utility to construct and operate on Hamiltonians from                    #'
-    )
-    print(
-        '#                 the Projections of DFT wfc on Atomic Orbital bases (PAO)                  #'
-    )
-    print(
-        '#                                                                                           #'
-    )
-    print(
-        '#############################################################################################\n'
-    )
+    logo = large if style == 'large' else small
+    print('\n' + logo)
