f748a9702bc8951e847e1fb01e1b3bd6e7417243
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-05-22 07:50:11 +0000

    Remove header

--- examples/TBmodel_examples/slater_koster.py
+++ examples/TBmodel_examples/slater_koster.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
 import matplotlib.pyplot as plt
-import numpy as np
-
-from PAOFLOW import PAOFLOW
+import numpy as npfrom PAOFLOW import PAOFLOW
 
 Ry2eV = 13.60569193
 
--- examples/acbn0_example/MgO/main.py
+++ examples/acbn0_example/MgO/main.py
@@ -1,27 +1,4 @@
-# *************************************************************************************
-# *                                                                                   *
-# *   PAOFLOW *  Marco BUONGIORNO NARDELLI * University of North Texas 2016-2024      *
-# *                                                                                   *
-# *************************************************************************************
-#
-#  Copyright 2016-2024 - Marco BUONGIORNO NARDELLI (mbn@unt.edu) - AFLOW.ORG consortium
-#
-#  This file is part of AFLOW software.
-#
-#  AFLOW is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# *************************************************************************************
+
 
 from PAOFLOW.ACBN0 import ACBN0
 
@@ -56,4 +33,3 @@ acbn0.optimize_hubbard_U(convergence_threshold=0.01)
 print('\nFinal U values:')
 for k,v in acbn0.uVals.items():
   print(f'  {k}: {v}')
-
