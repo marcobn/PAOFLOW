ab7de4dfa19aebee6be64cd9307e619fdddbd9c0
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-06-29 14:37:28 +0200

    Remove old Transport driver

--- src/PAOFLOW/transport/Transport.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility re-export for the transport sub-package import path.
-
-The public transport driver lives in ``PAOFLOW.Transport``.
-The conductor pipeline is in ``PAOFLOW.transport.conductor_pipeline``.
-"""
-
-from PAOFLOW.Transport import Transport
-
-__all__ = ['Transport']
