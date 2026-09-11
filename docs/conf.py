bc7119b959ae8d051309110f7a5605d13db95aa8
Zhiren <66028968+hzr-piggy@users.noreply.github.com>
2026-09-11 17:24:48 +0200

    version change

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1] / 'src'))
 
 project = 'PAOFLOW'
 author = 'PAOFLOW developers'
-release = '2.9.3'
+release = '3.0.0'
 
 extensions = [
     'myst_nb',
